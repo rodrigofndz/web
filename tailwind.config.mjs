f4715d28fec898aac67dd08b5b888d5ab60a99b5
rodrigofndz <fernandezsanchezr19@gmail.com>
2024-02-12 18:19:27 +0100

    profile image ratio fix & disabled animation
    update astro version
    
    updated astro version to 4.9
    
    i don't know what did i change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dark-blue': {
+					'50': '#ecf2ff',
+					'100': '#dce6ff',
+					'200': '#c0d1ff',
+					'300': '#9ab1ff',
+					'400': '#7286ff',
+					'500': '#515cff',
+					'600': '#3532f9',
+					'700': '#2c26dc',
+					'800': '#2522b4',
+					'900': '#24258b',
+					'950': '#171551',
+				},				
+			},
+		},
 	},
 	plugins: [],
 }
